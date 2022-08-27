e7197e5af5bcf8e01f26f7d0ee5ab407025b045d
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-08-27 13:42:31 +0530

    feat: filtering products completed

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,12 +1,37 @@
 import { RequestHandler } from "express";
+import Product from "../models/product";
 
 const getAlProductsStatic: RequestHandler = async (req, res) => {
-  throw new Error("testing async Error");
-  res.status(200).json({ msg: "products testing route" });
+  const search = "ab";
+  const products = await Product.find({
+    name: { $regex: search, $options: "i" },
+  });
+  res.status(200).json({ products });
 };
 
+interface QueryObject {
+  featured?: boolean;
+  company?: string;
+  name?: any;
+}
+
 const getAllProducts: RequestHandler = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const { featured, company, name } = req.query;
+  const queryObject: QueryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company as string;
+  }
+  if (name) {
+    queryObject.name = { $regex: name as string, $options: "i" };
+  }
+  console.log(queryObject);
+
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products });
 };
 
 export { getAlProductsStatic, getAllProducts };
